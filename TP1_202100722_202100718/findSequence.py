a0d77d00da031d45ebf5a320c17f2fdd85e9ff0d
JoaoFernandes02 <joaomlfernandes02@gmail.com>
2024-04-23 23:30:53 +0100

    fixed a bug
    os restos podiam ter 0 e n ser 0 (por exemplo 10)

--- TP1_202100722_202100718/findSequence.py
+++ TP1_202100722_202100718/findSequence.py
@@ -4,26 +4,26 @@ from functools import reduce
 def numeroIteracoes(primos):
     return reduce(lambda x, y: x * y, primos, 1)
 
-def stringResto(arrPrimos, i):
-    return ''.join(str(i % primo) for primo in arrPrimos)
+def arrayResto(arrPrimos, i):
+    return [(i % primo) for primo in arrPrimos]
 
 #primos até ao que queremos incluir nos saltos
 arrPrimos = [2,3,5,7]
 
 it = numeroIteracoes(arrPrimos)
 
-restos = [stringResto(arrPrimos, i) for i in range(arrPrimos[-1], it + arrPrimos[-1])]
+restos = [arrayResto(arrPrimos, i) for i in range(arrPrimos[-1], it + arrPrimos[-1])]
 saltos = [0] * len(restos)    
     
 restoSelecionado = 0
 
-while("0" in restos[restoSelecionado]):
+while(0 in restos[restoSelecionado]):
         restoSelecionado += 1
 
 while(saltos[restoSelecionado] == 0):
     proximoSalto = 1
     
-    while("0" in restos[(restoSelecionado + proximoSalto) % len(restos)]):
+    while(0 in restos[(restoSelecionado + proximoSalto) % len(restos)]):
         proximoSalto += 1
 
     saltos[restoSelecionado] = proximoSalto
