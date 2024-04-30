352f111ab79de9ade9fb962864432eb4286b4e73
Rodrigo Santos <rodrigo_santos003@outlook.com>
2024-04-30 18:01:04 +0100

    Minor changes

--- TP1_202100722_202100718/main.py
+++ TP1_202100722_202100718/main.py
@@ -5,19 +5,21 @@ import time
 
 def is_prime(n):
     """
-    Check if a number is prime, using trial division.
+    Check if a number is prime, using the form 6k+-1.
     :param n: Number to check
     :return: True if the number is prime, False otherwise
     """
     if n <= 1:
         return False
-    if n == 2 or n == 3:
+    elif n <= 3:
         return True
-    if n % 2 == 0:
+    elif n % 2 == 0 or n % 3 == 0:
         return False
-    for i in range(3, int(math.sqrt(n)) + 1, 2):
-        if n % i == 0:
+    i = 5
+    while i * i <= int(math.sqrt(n)):
+        if n % i == 0 or n % (i + 2) == 0:
             return False
+        i += 6
     return True
 
 
@@ -56,7 +58,7 @@ if __name__ == '__main__':
         num_processes = 8
 
         # Timeout for each worker
-        worker_timeout = 30
+        worker_timeout = 60
 
         # Multiprocessing pool
         with multiprocessing.Pool(num_processes) as pool:
@@ -64,9 +66,9 @@ if __name__ == '__main__':
                          [(worker_timeout,
                            max_prime,
                            smp_lock,
-                           (1 + x * (100 ** x)),  # Start value
-                           (100 ** x))  # Step
-                          for x in range(num_processes)])
+                           (1 + p * (100 ** p)),  # Start value
+                           (100 ** p))  # Step
+                          for p in range(num_processes)])
 
         # Print the maximum prime number found
-        print(max_prime.value, "(", len(str(max_prime.value)), ")")
+        print(f"{max_prime.value} ({len(str(max_prime.value))})")
