922e270f3e3e53f8ff5bc455731c0a92b84b6f17
Rodrigo Santos <rodrigo_santos003@outlook.com>
2024-05-23 09:47:25 +0100

    Solved server tests

--- TP2_202100722_202100718/server.py
+++ TP2_202100722_202100718/server.py
@@ -3,9 +3,10 @@
 
 """
 
-import functions
 import json
 import socket
+import inspect
+import functions
 
 
 class JSONRPCServer:
@@ -27,7 +28,7 @@ class JSONRPCServer:
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((self.host, self.port))
         self.sock.listen(1)
-        print('Listening on port %s ...' % self.port)
+        print(f'Listening on port {self.port} ...')
 
         try:
             while True:
@@ -47,6 +48,61 @@ class JSONRPCServer:
         """Stops the server."""
         self.sock.close()
 
+    def process_msg(self, msg):
+        """Process the request message and build a response"""
+        res = {'jsonrpc': '2.0'}
+
+        try:
+            # Load the json from the request
+            msg = json.loads(msg)
+
+            # Check if the request is valid
+            if 'method' not in msg:
+                raise ValueError('Invalid Request')
+            method = msg['method']
+
+            # Check if the request is a JSON-RPC notification
+            if 'id' not in msg:
+                return None
+            rpc_id = msg['id']
+
+            # Check if the method exists
+            if method not in self.funcs:
+                raise KeyError('Method not found')
+
+            # Check function arguments
+            func = self.funcs[method]
+            func_params = inspect.signature(func).parameters
+            if len(func_params) > 0:
+                if 'params' in msg:
+                    params = msg['params']
+
+                    if len(params) == len(func_params):
+                        res['result'] = func(*params)
+                    else:
+                        raise TypeError('Invalid params')
+                else:
+                    raise TypeError('Invalid params')
+            else:
+                res['result'] = func()
+
+            # Place the id on the response
+            res['id'] = rpc_id
+        except json.JSONDecodeError:
+            res['id'] = None
+            res['error'] = {'code': -32700, 'message': 'Parse error'}
+        except ValueError:
+            res['id'] = None
+            res['error'] = {'code': -32600, 'message': 'Invalid Request'}
+        except KeyError:
+            res['id'] = msg['id']
+            res['error'] = {'code': -32601, 'message': 'Method not found'}
+        except TypeError:
+            res['id'] = msg['id']
+            res['error'] = {'code': -32602, 'message': 'Invalid params'}
+
+        return json.dumps(res)
+
     def handle_client(self, conn):
         """Handles the client connection."""
 
@@ -54,18 +110,12 @@ class JSONRPCServer:
         msg = conn.recv(1024).decode()
         print('Received:', msg)
 
-        # Prepare response
-        res = {
-            'hello': 'World'
-        }
-
-        # Send response
-        res = json.dumps(res)
-        conn.send(res.encode())
+        res = self.process_msg(msg)
+        if res is not None:
+            conn.send(res.encode())
 
 
 if __name__ == "__main__":
-
     # Test the JSONRPCServer class
     server = JSONRPCServer('0.0.0.0', 8000)
 
